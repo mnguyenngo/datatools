66105383a9ac7cbff9a8d3cc31f7896755722d2c
Nguyen Ngo <mnguyenngo@gmail.com>
2018-08-08 16:49:24 -0700

    Updated module setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    reqs = f.read()
+
 setup(
     name='datatools',
     version='0.1',
     description='Helper functions for processing raw data',
-    url='https://github.com/mnguyenngo/ds-tools',
+    url='https://github.com/mnguyenngo/datatools',
     author='Nguyen Ngo',
     author_email='mnguyenngo@gmail.com',
     license='MIT',
     packages=['datatools'],
-    install_requires=['datetime']
+    install_requires=[reqs.strip().split('\n')]
 )
